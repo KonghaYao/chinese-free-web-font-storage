63c17d8aeddc89bc99c0c0c48e535eab33da9ae3
KonghaYao <20192831006@m.scnu.edu.cn>
2021-10-26 08:53:08 +0800

    feat: index.js 添加批量功能

--- index.js
+++ index.js
@@ -9,41 +9,61 @@ import genTestHTML from "./genTestHTML.js";
  * 3. 填写下面的 fontFileName 和license 路径
  */
 
-const fontFileName = "站酷庆科黄油体";
-const license = "./fonts/站酷庆科黄油体使用声明.txt"; // 字体版权说明文件， 有则填路径即可
-
-//======== 下面的代码请勿修改 =========
+const fontFileName = "站酷仓耳渔阳体-W05";
+const license = "./fonts/站酷仓耳渔阳体使用声明.txt"; // 字体版权说明文件， 有则填路径即可
 const FontPath = `./fonts/${fontFileName}.ttf`;
 
-fse.emptyDirSync("./build");
-fontSplit({
-    FontPath, // 字体位置
-    destFold: `./build/${fontFileName}`, // 必须设置为名称
-    css: {
-        fontStyle: "normal",
-        fontWeight: "normal",
-        fontDisplay: "swap",
-        fontFamily: null, // 如果不设置的话将会使用默认的字体名称哦
+// 单个字体文件的模板是这样子的，但是如果你想要批量的话，自己慢慢填就是了
+create([
+    {
+        fontFileName,
+        FontPath,
+        license,
     },
-    chunkOptions: {
-        allowEn: false,
-        totalSize: 6000, // 总共会抽取的字符数
-        chunkSize: 600, // 每个分段的字符数
-    },
-})
-    .then(({ fontFamily }) => {
-        return genTestHTML(fontFamily, fontFileName);
-    })
-    .then(() => console.log("测试文件生产完毕"))
-    .then(() => {
-        if (license) {
-            return fse
-                .outputFile(
-                    resolve(
-                        `./build/${fontFileName}/license` + extname(license)
-                    ),
-                    fse.readFileSync(license)
-                )
-                .then(() => console.log("声明文件生成完毕"));
+]).then(() => {
+    console.log("全部完成");
+});
+function create(fontArray) {
+    fse.emptyDirSync("./build");
+    //======== 下面的代码请勿修改 =========
+    const promises = fontArray.map(
+        ({ fontFileName, FontPath, license }, index) => {
+            return fontSplit({
+                FontPath, // 字体位置
+                destFold: `./build/${fontFileName}`, // 必须设置为名称
+                css: {
+                    fontStyle: "normal",
+                    fontWeight: "normal",
+                    fontDisplay: "swap",
+                    fontFamily: null, // 如果不设置的话将会使用默认的字体名称哦
+                },
+                chunkOptions: {
+                    allowEn: false,
+                    totalSize: 6000, // 总共会抽取的字符数
+                    chunkSize: 600, // 每个分段的字符数
+                },
+            })
+                .then(({ fontFamily }) => {
+                    return genTestHTML(fontFamily, fontFileName);
+                })
+                .then(() => console.log(fontFileName + "--测试文件生产完毕"))
+                .then(() => {
+                    if (license) {
+                        return fse
+                            .outputFile(
+                                resolve(
+                                    `./build/${fontFileName}/license` +
+                                        extname(license)
+                                ),
+                                fse.readFileSync(license)
+                            )
+                            .then(() =>
+                                console.log(fontFileName + "--声明文件生成完毕")
+                            );
+                    }
+                })
+                .then(() => console.log(fontFileName + "--全部完成"));
         }
-    });
+    );
+    return Promise.all(promises);
+}
