42bdd21f7f1a4801de5480a864717444b31a6001
KonghaYao <20192831006@m.scnu.edu.cn>
2021-10-27 09:25:58 +0800

    docs:完善介绍网站

--- docs/index.js
+++ docs/index.js
@@ -1,3 +1,4 @@
+Vue.use(window.Toasted);
 new Vue({
     el: "#app",
     data() {
@@ -6,7 +7,27 @@ new Vue({
             config: {
                 test: "与之斯部他行出不上公成地会个时学了后日月以和有大于人国中是为在一年的",
             },
+            choose: {
+                fontIndex: 0,
+                license: "",
+                cssIndex: 0,
+            },
+            show: { overlay1: false, fontDetail: "font", overlay2: false },
         };
+    },
+    computed: {
+        codeTemplate() {
+            try {
+                return `<link rel="stylesheet" href="${
+                    this.fontDetail.css[this.choose.cssIndex].url
+                }">`;
+            } catch (e) {
+                return "err";
+            }
+        },
+        fontDetail() {
+            return this.fonts[this.choose.fontIndex] || {};
+        },
     },
     mounted() {
         fetch("./fonts.json")
@@ -17,5 +38,30 @@ new Vue({
                 });
             });
     },
-    methods: {},
+    methods: {
+        chooseFont({ index, type }) {
+            this.choose.fontIndex = index;
+            this.show.fontDetail = type;
+            this.show.overlay1 = true;
+            this.choose.cssIndex = 0;
+            if (type === "license") this.getLicense(index);
+        },
+        copy(text) {
+            window.navigator.clipboard.writeText(text).then((res) => {
+                if (!res) {
+                    this.$toasted.show("复制完成");
+                }
+            });
+        },
+        chooseCSS(index) {
+            this.show.overlay2 = true;
+            this.choose.cssIndex = index;
+        },
+        async getLicense(index) {
+            const license = await fetch(this.fontDetail.license.link).then(
+                (res) => res.text()
+            );
+            this.choose.license = license;
+        },
+    },
 });
