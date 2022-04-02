7e134dd3cd0ac57c052e1941145de6b117eb603f
konghayao <dongzhongzhidong@qq.com>
2022-04-02 11:03:19 +0800

    feat: 更新操作

--- index.js
+++ index.js
@@ -7,79 +7,100 @@ import { resolve, extname } from "path";
  * 2. 将字体文件放入 fonts 文件夹中， 如果有版权声明则放入
  * 3. 填写下面的 fontFileName 和license 路径
  */
-
-const fontFileName = "站酷仓耳渔阳体-W01";
-const license = "./fonts/站酷仓耳渔阳体使用声明.txt"; // 字体版权说明文件， 有则填路径即可
-const FontPath = `./fonts/${fontFileName}.ttf`;
-
+const base = {
+    chunkOptions: {
+        other: 1,
+        SC: 4,
+        TC: 1,
+    },
+    license: "./fonts/LXGWWenKai.txt",
+};
 // 单个字体文件的模板是这样子的，但是如果你想要批量的话，自己慢慢填就是了
 create([
     {
-        fontFileName,
-        FontPath,
-        license,
+        fontFileName: "LXGWWenKai-Bold",
+        FontPath: "./fonts/LXGWWenKai-Bold.ttf",
         css: {
-            fontFamily: "站酷仓耳渔阳体",
-            fontWeight: 100,
+            fontFamily: "LXGWWenKai",
+            fontWeight: 700,
         },
-        chunkOptions: {
-            other: 1,
-            SC: 4,
-            TC: 1,
+        ...base,
+    },
+    {
+        fontFileName: "LXGWWenKai-Light",
+        FontPath: "./fonts/LXGWWenKai-Light.ttf",
+        css: {
+            fontFamily: "LXGWWenKai",
+            fontWeight: 300,
         },
+        ...base,
+    },
+    {
+        fontFileName: "LXGWWenKai-Regular",
+        FontPath: "./fonts/LXGWWenKai-Regular.ttf",
+        css: {
+            fontFamily: "LXGWWenKai",
+            fontWeight: 400,
+        },
+        ...base,
     },
 ]).then(() => {
     console.log("全部完成");
 });
 
 function create(fontArray) {
-    fse.emptyDirSync(`./build/${fontFileName}`);
     //======== 下面的代码请勿修改 =========
-    const promises = fontArray.map(
+    const promises = fontArray.reduce(
         (
-            { fontFileName, FontPath, license, css, chunkOptions, charset },
-            index
+            promise,
+            { fontFileName, FontPath, license, css, chunkOptions, charset }
         ) => {
-            return fontSplit({
-                FontPath, // 字体位置
-                destFold: `./build/${fontFileName}`, // 必须设置为名称
-                css: {
-                    fontStyle: "normal",
-                    fontWeight: "normal",
-                    fontDisplay: "swap",
-                    fontFamily: null, // 如果不设置的话将会使用默认的字体名称哦
-                    ...css,
-                },
-                charset: {
-                    other: true,
-                    TC: true,
-                    SC: true,
-                    ...charset,
-                },
-                chunkOptions: {
-                    TC: 3,
-                    SC: 6,
-                    other: 1,
-                    ...chunkOptions,
-                },
-            })
-                .then(() => {
-                    if (license) {
-                        return fse
-                            .outputFile(
-                                resolve(
-                                    `./build/${fontFileName}/license` +
-                                        extname(license)
-                                ),
-                                fse.readFileSync(license)
-                            )
-                            .then(() =>
-                                console.log(fontFileName + "--声明文件生成完毕")
-                            );
-                    }
+            fse.emptyDirSync(`./build/${fontFileName}`);
+            return promise.then(() =>
+                fontSplit({
+                    FontPath, // 字体位置
+                    destFold: `./build/${fontFileName}`, // 必须设置为名称
+                    css: {
+                        fontStyle: "normal",
+                        fontWeight: "normal",
+                        fontDisplay: "swap",
+                        fontFamily: null, // 如果不设置的话将会使用默认的字体名称哦
+                        ...css,
+                    },
+                    charset: {
+                        other: true,
+                        TC: true,
+                        SC: true,
+                        ...charset,
+                    },
+                    chunkOptions: {
+                        TC: 3,
+                        SC: 6,
+                        other: 1,
+                        ...chunkOptions,
+                    },
                 })
-                .then(() => console.log(fontFileName + "--全部完成"));
-        }
+                    .then(() => {
+                        if (license) {
+                            return fse
+                                .outputFile(
+                                    resolve(
+                                        `./build/${fontFileName}/license` +
+                                            extname(license)
+                                    ),
+                                    fse.readFileSync(license)
+                                )
+                                .then(() =>
+                                    console.log(
+                                        fontFileName + "--声明文件生成完毕"
+                                    )
+                                );
+                        }
+                    })
+                    .then(() => console.log(fontFileName + "--全部完成"))
+            );
+        },
+        Promise.resolve()
     );
-    return Promise.all(promises);
+    return promises;
 }
