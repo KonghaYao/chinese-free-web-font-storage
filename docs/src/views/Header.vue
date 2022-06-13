3280ba6d034ae0784b80b32ea189c1c8c9326f3b
konghayao <dongzhongzhidong@qq.com>
2022-06-13 10:33:39 +0800

    feat: 添加链接完成

--- docs/src/views/Header.vue
+++ docs/src/views/Header.vue
@@ -5,7 +5,11 @@
 <template>
     <div class="flex flex-row p-4 bg-gray-800 text-white select-none">
         <div class="text-4xl Font-T">江夏尧的字体仓库</div>
-        <div class="flex-grow overflow-y-auto overflow-x-hidden"></div>
+        <div class="flex-grow overflow-y-auto overflow-x-hidden flex px-8">
+            <router-link :to="{ name: 'Home' }">中文字体</router-link>
+            <router-link to="/material-icons">Material Icon</router-link>
+
+        </div>
         <div class="Font-Q">纵享中文字体之美</div>
     </div>
 </template>
