07d8f11d9967cddecfadfada86e5a36a127f6e90
konghayao <dongzhongzhidong@qq.com>
2022-06-15 08:27:14 +0800

    style: 修复样式问题

--- docs/src/views/Footer.vue
+++ docs/src/views/Footer.vue
@@ -1,6 +1,6 @@
  <template>
      <footer class="flex Font-E p-4 justify-between items-center bg-gray-100">
-         <div class="flex h-full">
+         <div class="h-full hidden md:flex">
              <div class="text-sm text-right">
                  Design By KonghaYao
                  <br />Please give me a star
@@ -10,6 +10,12 @@
                  江夏尧设计
                  <br />请给我一个赞吧
              </div>
+         </div>
+         <div class="flex-grow justify-center text-sm overflow-y-auto text-white overflow-x-hidden flex px-8">
+             <router-link class="Font-Q px-2 bg-lime-400 " :to="{ name: 'Home' }">中文字体</router-link>
+             <router-link class="Font-E px-2 bg-sky-400" to="/material-icons">Material Icon</router-link>
+             <router-link class="Font-E px-2 bg-orange-400" to="/google-fonts">Google Fonts</router-link>
+ 
          </div>
          <div class="flex flex-col">
              <div class="flex justify-evenly items-center">
--- docs/src/views/Header.vue
+++ docs/src/views/Header.vue
@@ -4,14 +4,11 @@
 
 <template>
     <div class="flex flex-row p-4 bg-gray-800 text-white select-none">
-        <div class="text-4xl Font-T">江夏尧的字体仓库</div>
-        <div class="flex-grow text-sm overflow-y-auto text-white overflow-x-hidden flex px-8">
-            <router-link class="Font-Q px-2 bg-lime-400 " :to="{ name: 'Home' }">中文字体</router-link>
-            <router-link class="Font-E px-2 bg-sky-400" to="/material-icons">Material Icon</router-link>
-            <router-link class="Font-E px-2 bg-orange-400" to="/google-fonts">Google Fonts</router-link>
+        <div class="flex-none text-4xl Font-T">江夏尧的字体仓库</div>
+        <div class="flex-grow w-full">
 
         </div>
-        <div class="Font-Q">纵享中文字体之美</div>
+        <div class="flex-none Font-Q">纵享中文字体之美</div>
     </div>
 </template>
 
