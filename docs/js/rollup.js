5d2211bfd5db2832951b1b4ecfd6a188aa68e47a
konghayao <dongzhongzhidong@qq.com>
2022-06-26 10:54:35 +0800

    feat: 提升版本

--- docs/js/rollup.js
+++ docs/js/rollup.js
@@ -1,92 +1,10 @@
-const CDN = globalThis.location.href;
 // 导入打包产物
-import { Compiler, Evaluator, sky_module, PluginLoader } from "rollup-web";
-import { vue } from "rollup-web/dist/plugins/vue3.js";
-import { babelCore } from "rollup-web/dist/plugins/babel.core.js";
-
-// 导入各种插件
-import typescript from "https://esm.sh/@babel/preset-typescript";
-
-const { default: json } = await PluginLoader.load("plugin-json");
-const { default: alias } = await PluginLoader.load("plugin-alias");
-const { postcss } = await PluginLoader.load("postcss");
-
-const config = {
-    plugins: [
-        json(),
-        alias({
-            entries: [{ find: "@", replacement: "./" }],
-        }),
-
-        babelCore({
-            babelrc: {
-                presets: [
-                    [
-                        typescript,
-                        {
-                            // 需要使用这种方式兼容 solid 配置
-                            isTSX: true,
-                            allExtensions: true,
-                        },
-                    ],
-                ],
-            },
-            extensions: [".ts"],
-            log(id) {
-                console.log("%cBabel typescript > " + id, "color:orange");
-            },
-        }),
-        postcss({
-            filter(url) {
-                // console.warn(url);
-                return true;
-            },
-            // log(id, css) {
-            //     console.warn(id, css);
-            // },
-        }),
-        // css(),
-        vue({}),
-        sky_module({
-            cdn: (name) => `https://fastly.jsdelivr.net/npm/${name}/+esm`,
-            rename: {
-                pinia: "pinia@2.0.11/dist/pinia.esm-browser.js/+esm",
-                "vue-router":
-                    "vue-router@4.0.12/dist/vue-router.esm-browser.js",
-                "@vue/devtools-api": "@vue/devtools-api/+esm",
-                vue: "vue@3.2.25/dist/vue.runtime.esm-browser.js",
-            },
-        }),
-        {
-            resolveId(thisFile) {
-                if (thisFile.startsWith("http")) {
-                    return thisFile;
-                } else if (thisFile.endsWith(".png")) {
-                    const id = new URL(thisFile, CDN).toString();
-                    console.warn(id);
-                    return {
-                        external: true,
-                        id,
-                    };
-                }
-            },
-        },
-    ],
-    external: ["vue", "vue-router", "pinia"],
-};
-
-const compiler = new Compiler(config, {
-    root: CDN,
-    extensions: [".vue", ".ts", ".js", ".json"],
-    log(url) {
-        console.log("%cDownload " + url, "color:green");
-    },
-    useDataCache: {},
-});
+import { Evaluator } from "rollup-web";
+// import { Evaluator } from "../rollup-web/dist/index.js";
 
 const Eval = new Evaluator();
-await Eval.createEnv({
-    Compiler: compiler,
-    root: CDN,
-});
+await Eval.useWorker("./js/bundle_worker.js");
+await Eval.createEnv();
+console.time();
 await Eval.evaluate("./src/main.ts");
+console.timeEnd();
