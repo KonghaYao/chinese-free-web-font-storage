53fe93cdd499e34ddfc86bbc72e7b1655b54331c
konghayao <3446798488@qq.com>
2022-11-20 08:49:46 +0800

    fixed: 修复网页打开问题

--- src/page/FontDetails.tsx
+++ src/page/FontDetails.tsx
@@ -13,6 +13,7 @@ export const FontDetails = () => {
     });
 
     initFontStore().then(async (api) => {
+        await api.loadFontList();
         await api.loadSingleFont();
         await api.ChangeFont();
     });
@@ -165,8 +166,8 @@ const BG: Component<{ class: string }> = (props) => {
     const text =
         '景建传积寸严尽清川止诚基而德谷恶宝曰则温盛容初所仕去圣空因非君忠兴之映笃业登棠赞作正祸璧事力夙松取定荣优甘诗念表听尺父竭薄如安令学以克端习庆资当履渊辞宜竞存丝贤形堂善竞孝深斯息言终无政悲维立虚缘是敬临兰不思慎甚从墨行名声福阴与命似流若美籍取';
     return (
-        <div class={'flex rounded-2xl p-8 px-16 ' + props.class}>
-            <div class="flex flex-col justify-evenly pr-8 text-9xl">
+        <div class={'mx-2  flex rounded-2xl p-8 px-8 sm:px-16 ' + props.class}>
+            <div class="flex flex-col justify-evenly pr-4  text-9xl sm:pr-8 ">
                 <div>永</div>
                 <div>远</div>
             </div>
