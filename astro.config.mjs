2625ebf1f977f330220381a4d87d0dffbc9f6ba4
江夏尧 <3446798488@qq.com>
2023-05-30 16:07:15 +0800

    feat: 添加 sentry 监控

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,14 @@ import solidJs from '@astrojs/solid-js';
 import tailwind from '@astrojs/tailwind';
 import robotsTxt from 'astro-robots-txt';
 
-import compress from "astro-compress";
+import compress from 'astro-compress';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://chinese-font.netlify.app',
-  integrations: [sitemap(), solidJs(), tailwind(), robotsTxt(), compress()],
-  // output: 'server',
-  output: 'static'
+    site: 'https://chinese-font.netlify.app',
+    integrations: [sitemap(), solidJs(), tailwind(), robotsTxt(), compress()],
+    // output: 'server',
+    output: 'static',
+
+    compressHTML: true,
 });
