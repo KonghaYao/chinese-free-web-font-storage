8b384331359e62318b8fc2a62f09b53e09dd8fa6
江夏尧 <3446798488@qq.com>
2023-08-03 23:01:01 +0800

    feat: 添加友链；优化布局

---
+++ src/configs.ts
@@ -0,0 +1,15 @@
+export const URLLinkHelp: Record<string, { url: string; desc: string }> = {
+    fontSource: { url: 'https://fontsource.org', desc: '收集 Google Web 字体的网站' },
+    opentype: {
+        url: 'https://opentype.js.org/',
+        desc: 'Javascript OpenType 解析库',
+    },
+    CSSFeatureDemo: {
+        url: 'https://sparanoid.com/lab/opentype-features/',
+        desc: 'OpenType 字体特性的 CSS 演示',
+    },
+    FontDetail: {
+        url: 'https://wakamaifondue.com/',
+        desc: '在线检测字体信息网站',
+    },
+};
