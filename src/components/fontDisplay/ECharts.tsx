b1148067986c6d75d37f35c9ec76825765924b7f
江夏尧 <3446798488@qq.com>
2023-09-23 19:34:39 +0800

    refactor: 调整 echarts 的加载方式，使得其速度加快

--- src/components/fontDisplay/ECharts.tsx
+++ src/components/fontDisplay/ECharts.tsx
@@ -1,34 +1,9 @@
 import { isServer } from 'solid-js/web';
 import { atom } from '@cn-ui/reactive';
 import { onCleanup, onMount } from 'solid-js';
-import * as echarts from 'echarts/core';
-import {
-    TitleComponent,
-    ToolboxComponent,
-    TooltipComponent,
-    GridComponent,
-    LegendComponent,
-    DataZoomComponent,
-    MarkLineComponent,
-} from 'echarts/components';
-import { LineChart, BarChart } from 'echarts/charts';
-import { UniversalTransition } from 'echarts/features';
-import { SVGRenderer } from 'echarts/renderers';
-echarts.use([
-    TitleComponent,
-    ToolboxComponent,
-    TooltipComponent,
-    GridComponent,
-    DataZoomComponent,
-    LineChart,
-    LegendComponent,
-    BarChart,
-    MarkLineComponent,
-    SVGRenderer,
-    UniversalTransition,
-]);
-
-const renderSVGChart = (reporter: echarts.EChartsCoreOption, dom: HTMLElement | null = null) => {
+import { type EChartsCoreOption, type ECharts as _ECharts } from 'echarts/core';
+const renderSVGChart = async (reporter: EChartsCoreOption, dom: HTMLElement | null = null) => {
+    const { echarts } = await import('./registerEcharts');
     const myChart = echarts.init(dom, null, {
         renderer: 'svg',
         ssr: isServer,
@@ -41,19 +16,19 @@ const renderSVGChart = (reporter: echarts.EChartsCoreOption, dom: HTMLElement | 
 };
 
 export const ECharts = (props: {
-    options: echarts.EChartsCoreOption;
-    onReady?: (chart: echarts.ECharts) => void;
+    options: EChartsCoreOption;
+    onReady?: (chart: _ECharts) => void;
 }) => {
     const dom = atom<HTMLElement | null>(null);
     const info = {} as { innerHTML?: string };
-    let myChart: echarts.ECharts;
+    let myChart: _ECharts;
     if (isServer) {
-        myChart = renderSVGChart(props.options);
-        info.innerHTML = myChart!.renderToSVGString();
+        // myChart = renderSVGChart(props.options);
+        // info.innerHTML = myChart!.renderToSVGString();
     } else {
-        onMount(() => {
+        onMount(async () => {
             !isServer && (dom()!.innerHTML = '');
-            myChart = renderSVGChart(props.options, dom());
+            myChart = await renderSVGChart(props.options, dom());
             setTimeout(() => {
                 myChart.resize({
                     height: 400,
---
+++ src/components/fontDisplay/registerEcharts.ts
@@ -0,0 +1,27 @@
+import * as echarts from 'echarts/core';
+import {
+    TitleComponent,
+    ToolboxComponent,
+    TooltipComponent,
+    GridComponent,
+    LegendComponent,
+    DataZoomComponent,
+    MarkLineComponent,
+} from 'echarts/components';
+import { LineChart, BarChart } from 'echarts/charts';
+import { UniversalTransition } from 'echarts/features';
+import { SVGRenderer } from 'echarts/renderers';
+echarts.use([
+    TitleComponent,
+    ToolboxComponent,
+    TooltipComponent,
+    GridComponent,
+    DataZoomComponent,
+    LineChart,
+    LegendComponent,
+    BarChart,
+    MarkLineComponent,
+    SVGRenderer,
+    UniversalTransition,
+]);
+export { echarts }
