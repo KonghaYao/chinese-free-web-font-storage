c418e2c8d8cd084aa82b471f75eacde015f34a37
江夏尧 <3446798488@qq.com>
2023-10-05 20:37:09 +0800

    feat: 修复新版reporter.json 的问题

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,12 +1,12 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
 // 这里写全局的 CDN 模块定义转接
-declare module "https://cdn.jsdelivr.net/npm/@konghayao/cn-font-split*" {
-    export * from "@konghayao/cn-font-split/dist/browser/index.d.ts";
+declare module 'https://cdn.jsdelivr.net/npm/@konghayao/cn-font-split*' {
+    export * from '@konghayao/cn-font-split/dist/browser/index.d.ts';
 }
-declare module "@konghayao/opentype.js" {
-    export * from "@types/opentype.js/index";
+declare module '@konghayao/opentype.js' {
+    export * from '@types/opentype.js/index.d';
 }
-declare module "@minht11/solid-virtual-container" {
-    export const VirtualContainer: any
+declare module '@minht11/solid-virtual-container' {
+    export const VirtualContainer: any;
 }
