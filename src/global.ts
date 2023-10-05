4e21a437e093f9e24355ef3c1454bfd25640873b
江夏尧 <3446798488@qq.com>
2023-10-05 20:38:36 +0800

    feat: CDN 回归

--- src/global.ts
+++ src/global.ts
@@ -1,4 +1,4 @@
 /** 全部字体 CDN ，不需要加末尾斜杠 */
-export const AllCDN = ['https://192960944.r.cdn36.com/chinesefonts1']
+export const AllCDN = ['https://192960944.r.cdn36.com/chinesefonts1'];
 /** 默认的 CDN */
-export const __CDN__ = 'https://ik.imagekit.io/chinesefonts1'
+export const __CDN__ = AllCDN[0];
