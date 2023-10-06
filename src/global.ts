bc2a5560db7499a975964d0da735e93a311c078c
江夏尧 <3446798488@qq.com>
2023-10-06 10:33:59 +0800

    feat: update CDN Path

--- src/global.ts
+++ src/global.ts
@@ -1,4 +1,7 @@
 /** 全部字体 CDN ，不需要加末尾斜杠 */
-export const AllCDN = ['https://192960944.r.cdn36.com/chinesefonts1'];
+export const AllCDN = [
+    'https://192960944.r.cdn36.com/chinesefonts2',
+    'https://192960944.r.cdn36.com/chinesefonts1',
+];
 /** 默认的 CDN */
 export const __CDN__ = AllCDN[0];
