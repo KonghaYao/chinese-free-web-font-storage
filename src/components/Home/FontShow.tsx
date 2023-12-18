3353092b5986352bd3375c858a31122d4b479593
3446798488 <3446798488@qq.com>
2023-12-18 06:11:58 +0000

    feat(anime): 添加字体动画

--- src/components/Home/FontShow.tsx
+++ src/components/Home/FontShow.tsx
@@ -1,13 +1,28 @@
-import { DebounceAtom, ThrottleAtom, atom, reflect } from '@cn-ui/reactive';
-import { onMount } from 'solid-js';
+import { ThrottleAtom, atom } from '@cn-ui/reactive';
+import { createEffect, onMount } from 'solid-js';
 import { __CDN__ } from '../../global';
-
+import anime from 'animejs'
+const showAnime = () => {
+    return anime({
+        targets: '.showing-text span',
+        translateY: [150, 0],
+        opacity: [0, 1],
+        easing: 'spring(1, 80, 10, 0)',
+        duration: 300,
+        delay: anime.stagger(100) // increase delay by 100ms for each elements.
+    })
+}
+console.log(anime)
 export const FontShow = () => {
     const originFont = atom({
         url: __CDN__ + '/packages/jxzk/dist/江西拙楷/result.css',
         style: 'font-family:jiangxizhuokai',
     });
-    const font = ThrottleAtom(originFont, 1000);
+    const font = ThrottleAtom(originFont, 2000);
+    createEffect(() => {
+        font()
+        showAnime()
+    })
     const text = '中文网字计划\n带来网络\n中文的爱与和谐';
     onMount(() => {
         [...document.querySelectorAll<HTMLAnchorElement>('.display-font-show-hover')].forEach(
@@ -21,10 +36,10 @@ export const FontShow = () => {
     return (
         <div class="flex select-text flex-col justify-center " style={font().style}>
             <div class="text-sky-500">鼠标移动到左侧按钮，即可预览字体样式</div>
-            <div class="my-6 text-6xl" style={'line-height:1.3;'} contentEditable>
+            <div class="my-6 text-6xl showing-text" style={'line-height:1.3;'} contentEditable>
                 {text.split('').map((i) => {
                     if (i === '\n') return <br />;
-                    return <span>{i}</span>;
+                    return <span class='inline-block'>{i}</span>;
                 })}
             </div>
             <div class="text-purple-600">如果你喜欢这款字体，可以点击进入详情页</div>
