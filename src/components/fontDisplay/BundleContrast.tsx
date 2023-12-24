1cb66544d5e44cdf7a3a494ce435336d8bc8839b
江夏尧 <3446798488@qq.com>
2023-12-24 10:17:08 +0800

    fix(message): 修改部分描述

--- src/components/fontDisplay/BundleContrast.tsx
+++ src/components/fontDisplay/BundleContrast.tsx
@@ -1,27 +1,37 @@
 import prettyBytes from 'pretty-bytes';
 import { AsyncReporterLoader } from './AsyncReporterLoader';
-import { Show } from 'solid-js/web';
 
+/** 展示打包数据的信息 */
 export const BundleContrast = AsyncReporterLoader((props) => {
     const bundleMessage = props.reporter.bundleMessage;
     const env = props.reporter.env;
     return (
         <div class="rounded-lg bg-white p-4 ">
-            <h2 class="pb-4 text-lg">打包信息</h2>
-            <ul class="text-gray-600">
-                <li>原始文件大小 {prettyBytes(bundleMessage.originLength)}</li>
-                <li>TTF状态文件大小 {prettyBytes(bundleMessage.ttfLength)}</li>
+            <h2 class="pb-4 text-lg">原始信息</h2>
+            <ul class="select-text text-gray-600">
                 <li>
-                    打包后总大小 {prettyBytes(bundleMessage.bundledTotalLength)} 「
-                    {(
-                        (bundleMessage.bundledTotalLength * 100) /
-                        bundleMessage.originLength
-                    ).toFixed(2)}
-                    %」
+                    原始文件大小
+                    <span class="float-right">{prettyBytes(bundleMessage.originLength)}</span>
+                </li>
+                <li>
+                    TTF状态文件大小
+                    <span class="float-right">{prettyBytes(bundleMessage.ttfLength)}</span>
+                </li>
+                <li>
+                    打包后总大小
+                    <span class="float-right text-green-600">
+                        {prettyBytes(bundleMessage.bundledTotalLength)} 「
+                        {(
+                            (bundleMessage.bundledTotalLength * 100) /
+                            bundleMessage.originLength
+                        ).toFixed(2)}
+                        %」
+                    </span>
                 </li>
                 {'os' in env && (
                     <>
-                        <li>
+                        <li class="text-green-600">
+                            设备信息
                             {[
                                 env.os.version,
                                 env.device.architecture,
@@ -29,8 +39,12 @@ export const BundleContrast = AsyncReporterLoader((props) => {
                                 '核心CPU',
                             ].join(' ')}
                         </li>
-                        <li>{[env.envName, env.runtime.version].join(' ')}</li>
-                        <li>{ShowTimeFormat(new Date(env.createdTime))}</li>
+                        <li>
+                            {[env.envName, env.runtime.version].join(' ')}
+                            <span class="float-right">
+                                {ShowTimeFormat(new Date(env.createdTime))}
+                            </span>
+                        </li>
                     </>
                 )}
             </ul>
