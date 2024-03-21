0156ba9141f37c069c9a657cc61548dd0e1cbbe6
3446798488 <3446798488@qq.com>
2024-03-21 03:25:00 +0000

    feat(index): 使用更加可用的 index 生成方式

--- scripts/createIndex.mjs
+++ scripts/createIndex.mjs
@@ -1,11 +1,16 @@
 import { globSync } from "glob";
 import fs from "fs-extra";
+import p from 'path'
 const cn = await fs.readJSON("./overrides.json");
 const data = Object.fromEntries(
     Object.entries(cn).map(([en, cn]) => {
-        const path = globSync(`packages/${en}/**/result.css`).map((i) =>
-            i.replaceAll("\\", "/")
+        const path = globSync(`packages/${en}/fonts/*`).map((i) => {
+            const nation = i.replaceAll("\\", "/").replace('fonts', "dist")
+            const extName = p.extname(nation); // 获取文件后缀名，包含点号
+            return p.dirname(nation) + '/' + p.basename(nation, extName) + '/result.css'
+        }
         );
+        console.log(path)
         return [
             en,
             {
