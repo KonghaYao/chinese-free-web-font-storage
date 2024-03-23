5a1d74e82be30c01b90e73d0349bb684e80e1307
江夏尧 <3446798488@qq.com>
2024-03-23 14:23:24 +0800

    chore: update cdn link

--- src/global.ts
+++ src/global.ts
@@ -1,8 +1,7 @@
 /** 全部字体 CDN ，不需要加末尾斜杠 */
 export const AllCDN = [
+    'https://834832894.r.cdn36.com/chinesefonts3',
     'https://192960944.r.cdn36.com/chinesefonts3',
-    'https://192960944.r.cdn36.com/chinesefonts2',
-    'https://192960944.r.cdn36.com/chinesefonts1',
 ];
 /** 默认的 CDN */
-export const __CDN__ = 'https://834832894.r.cdn36.com/chinesefonts3';
+export const __CDN__ = 'https://325201388.r.cdn36.com/chinesefonts3';
