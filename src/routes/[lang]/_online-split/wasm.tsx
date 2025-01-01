83b6712415103f0cf54908db31d2d6d81fe63386
江夏尧 <3446798488@qq.com>
2025-01-01 08:09:56 +0800

    fix: 修复构建

--- src/routes/[lang]/_online-split/wasm.tsx
+++ src/routes/[lang]/_online-split/wasm.tsx
@@ -7,7 +7,7 @@ import { fontSplit, proto, StaticWasm } from 'cn-font-split-wasm';
 import { useZip } from './useZip';
 import { getTestingFile } from './getVersions';
 const wasm = new StaticWasm(
-    'https://ik.imagekit.io/github/KonghaYao/cn-font-split/releases/download/7.0.0-beta-1/cn-font-split-7.0.0-beta-1-wasm32-wasi.Oz.wasm'
+    'https://ik.imagekit.io/github/KonghaYao/cn-font-split/releases/download/7.0.1/libffi-wasm32-wasip1.wasm'
 );
 export default () => {
     const file = atom<File | null>(null);
