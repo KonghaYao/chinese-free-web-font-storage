ff50f65a6855aa74c6a2b2fb4e357a6319fc861c
江夏尧 <3446798488@qq.com>
2025-01-12 19:47:27 +0800

    feat: 添加 ZSFT 链接

--- src/configs.ts
+++ src/configs.ts
@@ -16,6 +16,10 @@ export const URLLinkHelp: Record<string, { url: string; desc: string }> = {
         url: 'https://www.netlify.com/',
         desc: 'Netlify - 部署服务提供商',
     },
+    ZSFT: {
+        url: 'https://fonts.zeoseven.com/',
+        desc: 'ZSFT - 国内字体 CDN | ZeoSeven™ Fonts',
+    },
 };
 
 /** 灵感页面的展示窗口 */
@@ -25,13 +29,13 @@ export const ShowCaseLinks = () => {
             title: '中华诗词大典',
             image: 'https://ik.imagekit.io/chinesefonts1/showcase/chinese_poetry.png?updatedAt=1704519777120&tr=w-1200%2Ch-630%2Cfo-auto',
             url: 'https://chinese-poetry.netlify.app/',
-            categories: [$t("8e964d494f3985b3f8c61ac27d2f0e7b")],
+            categories: [$t('8e964d494f3985b3f8c61ac27d2f0e7b')],
         },
         {
             title: '魔导绪论',
             image: 'https://ik.imagekit.io/chinesefonts1/showcase/magic_tag.png?updatedAt=1704519777120&tr=w-1200%2Ch-630%2Cfo-auto',
             url: 'https://magic-tag.netlify.app/',
-            categories: [$t("ddcd9b9d106dcb0f3f5690c76ec7bece")],
+            categories: [$t('ddcd9b9d106dcb0f3f5690c76ec7bece')],
         },
     ];
 };
