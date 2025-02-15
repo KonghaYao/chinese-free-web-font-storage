8890cab9bc92ea694ee2b90c9771c05ee1da9faf
江夏尧 <3446798488@qq.com>
2025-02-15 15:42:40 +0800

    chore: 更改依赖版本

--- app.config.ts
+++ app.config.ts
@@ -15,7 +15,7 @@ const { default: mdx } = pkg;
 export default defineConfig({
     extensions: ['mdx', 'md', 'tsx', 'ts'],
     server: {
-        preset: 'netlify_builder',
+        preset: 'netlify',
         esbuild: {
             options: {
                 target: 'esnext',
