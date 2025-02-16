d845c9a9c60ba1499259aa3c419d76779e3efb75
江夏尧 <3446798488@qq.com>
2025-02-16 20:42:28 +0800

    fix: 修复解析导出

--- src/utils/getFontReporter.ts
+++ src/utils/getFontReporter.ts
@@ -1,4 +1,4 @@
-import { decodeReporter } from 'cn-font-split';
+import { decodeReporter } from 'cn-font-split/dist/createAPI';
 import { __CDN__ } from '../global';
 const cache = new Map<string, Promise<any>>();
 export const getFontReporter = (font: string, fileName: string) => {
