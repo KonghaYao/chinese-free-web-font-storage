95b57b2d022add8d489fb7d8a0329f47e7da324b
江夏尧 <3446798488@qq.com>
2025-05-07 17:57:12 +0800

    fix: 修复

--- src/routes/[lang]/_cdn/SearchBox.tsx
+++ src/routes/[lang]/_cdn/SearchBox.tsx
@@ -75,7 +75,7 @@ export const SearchBox = () => {
                                             alt=""
                                         />
                                         <span
-                                            title={`https://chinese-fonts-cdn/${remote.url}}`}
+                                            title={`https://chinese-fonts-cdn.deno.dev/${remote.url}}`}
                                             class="flex justify-evenly border-t pt-1 text-xs text-blue-400"
                                         >
                                             <span>{font.id}</span>
@@ -83,7 +83,9 @@ export const SearchBox = () => {
                                             <span
                                                 class="cursor-pointer text-blue-600 transition-colors"
                                                 onclick={() => {
-                                                    copy(`https://chinese-fonts-cdn/${remote.url}`);
+                                                    copy(
+                                                        `https://chinese-fonts-cdn.deno.dev/${remote.url}`
+                                                    );
                                                     Notice.success('复制 CDN 地址成功');
                                                     copied(true);
                                                 }}
@@ -104,7 +106,7 @@ export const SearchBox = () => {
                                         <div class="flex-1 overflow-hidden flex flex-col text-xs text-left gap-1 text-neutral-600 px-2 bg-gray-100">
                                             <h4>{$t('a5eb7686aca0662121e7c4f32c2f5bee')}</h4>
                                             <LinkCode
-                                                href={`https://chinese-fonts-cdn/${remote.url}`}
+                                                href={`https://chinese-fonts-cdn.deno.dev/${remote.url}`}
                                             ></LinkCode>
                                             <h4>{$t('e780454184b9e6853e444d6f372e7272')}</h4>
                                             <FontCode style={remote.style}></FontCode>
